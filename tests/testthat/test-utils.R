34f5d48ad70c1a46d5fe2c6cde61d8f225177575
EllaKaye <ella.kaye@gmail.com>
2025-03-14 12:37:54 +0000

    test penguins_examples()

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# penguins_examples()
+test_that("penguins_examples with NULL lists files", {
+  expect_true("analysis/penguins.qmd" %in% penguins_examples())
+  expect_true("penguins_graph.R" %in% penguins_examples())
+})
+
+test_that("penguins_examples with file returns correct path", {
+  expect_true(file.exists(penguins_examples("penguins_graph.R")))
+  expect_true(file.exists(penguins_examples("analysis/penguins.qmd")))
+  expect_error(file.exists(penguins_examples("not_a_file.R")))
 })

cd5e047bb28c1af8e06a6147d7fe9862bc0ae1b2
EllaKaye <ella.kaye@gmail.com>
2025-03-21 14:14:33 +0000

    Simplify penguins.R in inst/extdata

--- inst/extdata/penguins.R
+++ inst/extdata/penguins.R
@@ -7,18 +7,7 @@ penguins |>
   select(body_mass_g, ends_with("_mm")) |>
   glimpse()
 
-# Scatterplot example 1: penguin flipper length versus body mass
+# Scatterplot example: penguin flipper length versus body mass
 ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
   geom_point(aes(color = species, shape = species), size = 2) +
   scale_color_manual(values = c("darkorange", "darkorchid", "cyan4"))
-
-# Scatterplot example 2: penguin bill length versus bill depth
-ggplot(data = penguins, aes(x = bill_length_mm, y = bill_depth_mm)) +
-  geom_point(aes(color = species, shape = species), size = 2) +
-  scale_color_manual(values = c("darkorange", "darkorchid", "cyan4"))
-
-# With facets
-ggplot(penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
-  geom_point(aes(color = sex)) +
-  scale_color_manual(values = c("darkorange", "cyan4"), na.translate = FALSE) +
-  facet_wrap(~species)

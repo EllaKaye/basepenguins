8e814a326d91f702564de44c1eae1418e4bf0781
EllaKaye <ella.kaye@gmail.com>
2025-03-31 07:36:02 +0100

    Simplifty penguins.R example file

--- inst/extdata/penguins.R
+++ inst/extdata/penguins.R
@@ -5,9 +5,6 @@ library(dplyr)
 # exploring scatterplots
 penguins |>
   select(body_mass_g, ends_with("_mm")) |>
-  glimpse()
-
-# Scatterplot example: penguin flipper length versus body mass
-ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g)) +
+  ggplot(aes(x = flipper_length_mm, y = body_mass_g)) +
   geom_point(aes(color = species, shape = species), size = 2) +
   scale_color_manual(values = c("darkorange", "darkorchid", "cyan4"))
